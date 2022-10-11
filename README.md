d2bb72e39d0b7b071254e775d5b09c47eaba0488
felipemassignan <114188078+felipemassignan@users.noreply.github.com>
2022-10-10 22:52:42 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# liguagem_do_L
+
+Description. 
+The package liguagem_do_L is used to:
+	- Trocar o R da frase por L
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install liguagem_do_L
+
+```bash
+pip install liguagem_do_L
+```
+
+## Usage
+
+```python
+from liguagem_do_L.trocar import elado
+elado.my_function()
+```
+
+## Author
+Felipe Massignan
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="liguagem_do_L",
+    version="0.0.1",
+    author="Felipe",
+    author_email="massigna@gmail.com",
+    description="Troca do 'R'pelo 'L'",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
